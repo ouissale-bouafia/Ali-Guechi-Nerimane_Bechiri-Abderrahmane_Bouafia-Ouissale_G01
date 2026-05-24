c2b41fcde4a12277855b2fe9d4179624330c4f69
ouissale-bouafia <ouissale.bouafia@univ-skikda.dz>
2026-05-24 13:33:45 +0100

    ouissales files
    i contributed in the xml, xquery files as well as the header of the website and concours page

---
+++ header.php
@@ -0,0 +1,32 @@
+<?php
+$xml = simplexml_load_file("C:/xampp/htdocs/projetksri/club.xml");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Info_Tech Club</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+
+    <div class="logo">
+        <h1>Info_Tech Club</h1>
+    </div>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="concours.php">Competitions</a>
+        <a href="inscription.php">Registration</a>
+        <a href="resultats.php">Results</a>
+        <a href="hendis.php">XQuery</a>
+    </nav>
+
+</header>
+
+<div class="container">
